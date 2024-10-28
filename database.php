fcd8e6575845d65612ff61ac83aac3fcd154468e
LorezoParis1996 <lorenzoparis86@live.it>
2024-10-28 11:56:45 +0100

    created table

--- database.php
+++ database.php
@@ -15,12 +15,32 @@ if ($conn->connect_error) {
 echo "Connection successful";
 
 //create db
-$sql = "CREATE DATABASE myDB";
+$sql = "CREATE DATABASE IF NOT EXISTS myDB"; //IF NOT EXISTS is needed when working on already existing database, otherwise it will try to create a new db that already exists and throw errors.
 
 if ($conn->query($sql) === true) {
     echo "Db creation successful";
 } else {
     echo "Error creating db: " . $conn->connect_error; 
+}
+
+//select db
+$conn->select_db("myDB");
+
+//create table
+$users_table = "CREATE TABLE users (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(40) NOT NULL,
+    lastname VARCHAR(40) NOT NULL,
+    age TINYINT UNSIGNED NOT NULL,
+    birth_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($users_table) === true) {
+    echo "Table creation successful";
+} else {
+    echo "Error creating table: " . $conn->connect_error;
 }
 
 ?>
